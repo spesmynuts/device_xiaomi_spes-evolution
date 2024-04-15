8b6b96b1f22db66f52218ed50b8674e4f8648ed7
tanvirr007 <md.tanvirhasannijum@gmail.com>
2024-04-15 17:11:42 +0600

    spes: configure vendorsetup.sh
    Signed-off-by: tanvirr007 <md.tanvirhasannijum@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -16,3 +16,10 @@ rm -rf hardware/lineage/compat/Android.bp
 echo -e "${color}Switch back to legacy imsrcsd sepolicy${end}"
 rm -rf device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/ims/imsservice.te
 cp device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/legacy-ims/hal_rcsservice.te device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/ims/hal_rcsservice.te
+
+# Vendor & Kernel Sources
+git clone https://github.com/tanvirr007/vendor_xiaomi_spes -b 14-FIRE vendor/xiaomi/spes
+git clone https://github.com/muralivijay/kernel_xiaomi_sm6225 kernel/xiaomi/sm6225
+
+# Lineage-21 Hardware Source
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
