b989806fd0dc5453c194628508cbd4cf8305e86c
Tanvir Hasan <43490582+tanvirr007@users.noreply.github.com>
2024-04-17 15:09:13 +0600

    spes: renamed vendor branch name
    I updated my vendor branch name, and it's connected with `vendorsetup.sh`. So, updating the branch name here is necessary.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,7 +18,7 @@ rm -rf device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/ims/imsservice.te
 cp device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/legacy-ims/hal_rcsservice.te device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/ims/hal_rcsservice.te
 
 # Vendor & Kernel Sources
-git clone https://github.com/tanvirr007/vendor_xiaomi_spes -b 14-FIRE vendor/xiaomi/spes
+git clone https://github.com/tanvirr007/vendor_xiaomi_spes -b 14.0 vendor/xiaomi/spes
 git clone https://github.com/muralivijay/kernel_xiaomi_sm6225 kernel/xiaomi/sm6225
 
 # Lineage-21 Hardware Source
