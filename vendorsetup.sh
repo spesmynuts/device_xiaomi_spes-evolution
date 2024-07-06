8f562d5c8a5eb8aec2844e93a4c67683d393a577
tanvirr007 <md.tanvirhasannijum@gmail.com>
2024-07-06 16:18:32 +0600

    spes: vendorsetup.sh: update kernel source
    Signed-off-by: tanvirr007 <tanvir@bombromgang.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,8 +18,10 @@ rm -rf device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/ims/imsservice.te
 cp device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/legacy-ims/hal_rcsservice.te device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/ims/hal_rcsservice.te
 
 # Vendor & Kernel Sources
+echo -e "${color}Cloning vendor & kernel from tanvirr007${end}"
 git clone https://github.com/tanvirr007/vendor_xiaomi_spes -b 14.0 vendor/xiaomi/spes
-git clone https://github.com/muralivijay/kernel_xiaomi_sm6225 kernel/xiaomi/sm6225
+git clone https://github.com/tanvirr007/kernel_xiaomi_sm6225 -b NaughtySilver kernel/xiaomi/sm6225
 
 # Lineage-21 Hardware Source
+echo -e "${color}Cloning Hardware from Lineage-21${end}"
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
