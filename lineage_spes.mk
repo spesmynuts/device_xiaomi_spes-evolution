cd8dd5964d2958c087fb2ae7a1e2ae452df7a6b1
tanvirr007 <md.tanvirhasannijum@gmail.com>
2024-10-11 16:39:46 +0600

    spes: Init @Evolution-X
    Signed-off-by: tanvirr007 <md.tanvirhasannijum@gmail.com>

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Inherit some common EuclidOS stuff.
-$(call inherit-product, vendor/euclid/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Product Specifics
-PRODUCT_NAME := euclid_spes
+PRODUCT_NAME := lineage_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
@@ -26,13 +26,11 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # Boot Animaton
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# EuclidOS Flags
-EUCLID_BUILD_TYPE := OFFICIAL
-EUCLID_MAINTAINER := тαиνir
-EUCLID_GAPPS := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_PIXEL_LAUNCHER := true
-TARGET_SUPPORTS_TOUCHGESTURES := true
+# EvoX Stuff
+WITH_GMS := true
+EVO_BUILD_TYPE = Unofficial
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
